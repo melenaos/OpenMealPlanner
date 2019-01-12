0d815f4c9ca60863f101f471d104a25d685395cd
Menelaos Vergis <melenaos@outlook.com>
2019-01-12 16:05:44 +0200

    Close sidenav overlay on changing pages, align center the 'My kitchen' link

--- src/layout/KitchenLibraryMenu.js
+++ src/layout/KitchenLibraryMenu.js
@@ -9,25 +9,30 @@ const selectedMenu = {
 }
 //<li><SearchLibrary searchValue="" onSearchChange={searchChange} /></li>
 class KitchenLibraryMenu extends Component {
+   
     componentDidMount() {
         var elem = document.querySelector(".sidenav")
-        M.Sidenav.init(elem, {
+         M.Sidenav.init(elem, {
             edge: "left",
             inDuration: 250
         })
     }
 
     render() {
+        // Add className="sidenav-close" to every link that change page.
+        // Materialize sidenav creates an overlay outside of #root and thus React-Route doesn't removes it
+        //  when rerenders the page.
+        // The fix is to close the sidenav when a page changes so the addon closes the overlay and NavLink change the page
         return (
             <ul id="kitchen-menu-sidebar" className={"sidenav kitchen-menu orange darken-3 " + (this.props.fullwidth ? "" : "sidenav-fixed")} >
 
                 <li><a className="header">Kitchen library</a></li>
-                <li><NavLink activeStyle={selectedMenu} to="/user/kitchen-library/all-recipes">All recipes</NavLink></li>
-                <li><NavLink activeStyle={selectedMenu} to="/user/kitchen-library/my-recipes">My recipes</NavLink></li>
-                <li><NavLink activeStyle={selectedMenu} to="/user/kitchen-library/meal-planning">Meal planning</NavLink></li>
+                <li className="sidenav-close"><NavLink activeStyle={selectedMenu} to="/user/kitchen-library/all-recipes">All recipes</NavLink></li>
+                <li className="sidenav-close"><NavLink activeStyle={selectedMenu} to="/user/kitchen-library/my-recipes">My recipes</NavLink></li>
+                <li className="sidenav-close"><NavLink activeStyle={selectedMenu} to="/user/kitchen-library/meal-planning">Meal planning</NavLink></li>
                 <li><div className="divider"></div></li>
-                <li><NavLink activeStyle={selectedMenu} to="/user/settings">Settings</NavLink></li>
-                <li><Link to="/login" onClick={this.props.signOut}>Logout</Link></li>
+                <li className="sidenav-close"><NavLink activeStyle={selectedMenu} to="/user/settings">Settings</NavLink></li>
+                <li className="sidenav-close"><Link to="/login" onClick={this.props.signOut}>Logout</Link></li>
             </ul>)
     }
 }
